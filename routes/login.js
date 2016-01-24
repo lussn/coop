7c1dd6b375edae315a52fe669c88493f0202feea
Luis R. Palanca <luis@typeform.com>
2016-01-24 18:13:52 +0100

    Login error message

--- routes/login.js
+++ routes/login.js
@@ -6,8 +6,15 @@ router.get('/login', function(req, res) {
     res.render('login', { user : req.user });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
+router.post('/login', function(req, res, next){
+    passport.authenticate('local', function(err, user, info) {
+      if (err) { return next(err); }
+      if (!user) { return res.render('login', {info: 'Invalid username or password. Try again.'}); }
+      req.login(user, function(err) {
+      	if (err) { return next(err); }
+      	return res.redirect('/');
+      });
+    })(req, res, next);   
 });
 
 router.get('/logout', function(req, res) {
