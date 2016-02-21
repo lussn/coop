1e000b5bbae199faee2a4216d432391bc28a312a
Luis R. Palanca <luis@typeform.com>
2016-02-21 13:29:54 +0100

    Trying bootstrap table styles

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,14 @@ var ReactDOM = require('react-dom');
 var CrudTable = React.createClass({
   render: function() {
     return (
-      <table className="table">
+      <table className="table table-hover table-bordered">
+      <thead>
+        <tr><th>Cooperative</th><th>Members</th><th>Actions</th></tr>
+      </thead>
       <tbody>
-        <tr><th>Cooperative</th><th>Edit</th><th>Delete</th></tr>
-        <tr><td>Una</td><td>Edit</td><td>Delete</td></tr>
-        <tr><td>Dos</td><td>Edit</td><td>Delete</td></tr>
-        <tr><td>Tres</td><td>Edit</td><td>Delete</td></tr>
+        <tr><td>Una</td><td>12</td><td>Delete</td></tr>
+        <tr><td>Dos</td><td>10</td><td>Delete</td></tr>
+        <tr><td>Tres</td><td>0</td><td>Delete</td></tr>
       </tbody>
       </table>
     );
