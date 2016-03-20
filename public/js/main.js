de90738332cdd6905d11c8bbd933f350cdc26d11
Luis R. Palanca <luis@typeform.com>
2016-03-20 18:51:32 +0100

    [#8] Prepare for single page app

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Table from './Table.jsx'
 
+function _getOrganizationsTable() {
+  return <Table changeTablePage={this.changeTablePage} />
+}
+
 class App extends Component {
+  pages = {
+    'organizations': _getOrganizationsTable
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      page: 'organizations'
+    }
+  }
+
+  changeTablePage = function (page) {
+    this.setState({
+      page: page
+    })
+  }
+
   render () {
-    return (
-      <Table/>
-    );
+    return this.pages[this.state.page].call(this)
   }
 }
 
