603a66ade92bf925a7ce95eb2593fb3352301bff
Luis R. Palanca <luis@typeform.com>
2016-05-27 14:58:52 +0200

    [#17] Create account integration tests

--- test/Account.test.js
+++ test/Account.test.js
@@ -3,41 +3,41 @@ var mongoose = require('mongoose');
 var Account = require('../infrastructure/persistence/authentication/Account.js');
 var db;
 
-describe('Account', function() {
+describe('Account', function () {
 
-    before(function(done) {
-        db = mongoose.connect('mongodb://localhost/test');
-            done();
+  before(function (done) {
+    db = mongoose.connect('mongodb://localhost/test');
+    done();
+  });
+
+  after(function (done) {
+    mongoose.connection.close();
+    done();
+  });
+
+  beforeEach(function (done) {
+    var account = new Account({
+      username: '12345',
+      password: 'test',
+      email: 'test@test.com'
     });
 
-    after(function(done) {
-        mongoose.connection.close();
-        done();
+    account.save(function (error) {
+      done();
     });
+  });
 
-    beforeEach(function(done) {
-        var account = new Account({
-            username: '12345',
-            password: 'test',
-            email: 'test@test.com'
-        });
-
-        account.save(function(error) {
-            done();
-        });
+  it('find a user by username', function (done) {
+    Account.findOne({username: '12345'}, function (err, account) {
+      account.username.should.eql('12345');
+      done();
     });
+  });
 
-    it('find a user by username', function(done) {
-        Account.findOne({ username: '12345' }, function(err, account) {
-            account.username.should.eql('12345');
-            done();
-        });
+  afterEach(function (done) {
+    Account.remove({}, function () {
+      done();
     });
-
-    afterEach(function(done) {
-        Account.remove({}, function() {
-            done();
-        });
-     });
+  });
 
 });
---
+++ test/AccountsRepository.integration.test.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var Account = require('../infrastructure/persistence/authentication/Account.js');
+var AccountsRepository = require("../infrastructure/persistence/AccountsRepository.js");
+var assert = require('chai').assert;
+var db;
+
+const NEW_USERNAME = 'testUsername';
+
+function assertAccountHasNewUsername(accounts) {
+  assert.equal(NEW_USERNAME, accounts.username);
+}
+describe('AccountsRepository', function() {
+
+  before(function (done) {
+    db = mongoose.connect('mongodb://localhost/test');
+    done();
+  });
+
+  after(function (done) {
+    mongoose.connection.close();
+    done();
+  });
+
+  beforeEach(function (done) {
+    var account = new Account({
+      username: '12345',
+      password: 'test',
+      email: 'test@test.com'
+    });
+
+    account.save(function (error) {
+      done();
+    });
+  });
+
+  it('Update should return updated account', function (done) {
+    Account.findOne({username: '12345'}, function (err, account) {
+      AccountsRepository.update(account._id, {
+        username: NEW_USERNAME,
+        password: account.password,
+        email: account.email
+      }, function(error, accounts) {
+        assertAccountHasNewUsername(accounts);
+        done();
+      });
+    });
+  });
+});
