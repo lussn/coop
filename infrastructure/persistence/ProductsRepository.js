6b962a7cf4e949aba6c2ba9927dd61b9fed485b5
Luis R. Palanca <luis@typeform.com>
2016-10-18 17:30:09 +0200

    [#23] save product repository

--- infrastructure/persistence/ProductsRepository.js
+++ infrastructure/persistence/ProductsRepository.js
@@ -1,6 +1,12 @@
 var ProductPersistenceSchema = require('./schemas/ProductPersistenceSchema.js');
 
 var ProductsRepository = function ProductsRepository() {
-
+  this.save = function (item, ownerId) {
+    var product = ProductPersistenceSchema.createPersistenceModel(
+      item,
+      ownerId
+    );
+    return product.save();
+  }
 };
 module.exports = new ProductsRepository();
